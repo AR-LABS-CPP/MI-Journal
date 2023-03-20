86269ef05b0858e167dc784ca1b13dc99530c2c9
AR-LABS-CPP <aliraza.abro.hyd@gmail.com>
2023-03-20 20:18:30 +0500

    Added login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import ErrorPage from "./pages/ErrorPage"
 import LandingPage from "./pages/LandingPage"
+import LoginPage from "./pages/LoginPage"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+    errorElement: <ErrorPage />
+  }
+])
 
 const App = () => {
   return (
-    <LandingPage />
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+    return (
+        <div className="min-h-screen flex justify-center items-center">
+            <p className="text-4xl">Oops! an error occured, please try again.</p>
+        </div>
+    )
+}
+
+export default ErrorPage
