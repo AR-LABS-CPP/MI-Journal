312b8f2688da76e740b518c2e33c675ceb440c61
AR-LABS-CPP <aliraza.abro.hyd@gmail.com>
2023-03-20 21:39:46 +0500

    Added new screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import NavBar from "./components/NavBar"
+import Blogs from "./pages/Blogs"
+import CreateBlog from "./pages/CreateBlog"
 import ErrorPage from "./pages/ErrorPage"
 import LandingPage from "./pages/LandingPage"
 import LoginPage from "./pages/LoginPage"
@@ -6,12 +9,27 @@ import LoginPage from "./pages/LoginPage"
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <LandingPage />,
+    element: <NavBar><LandingPage /></NavBar>,
     errorElement: <ErrorPage />
   },
   {
     path: "/login",
     element: <LoginPage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/blogs",
+    element: <NavBar><Blogs /></NavBar>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/blog",
+    element: <NavBar><Blogs /></NavBar>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/blogs/new",
+    element: <NavBar><CreateBlog /></NavBar>,
     errorElement: <ErrorPage />
   }
 ])
